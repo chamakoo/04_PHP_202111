34fd7109b1c581e5b6d550dd9b7c78b3a561390d
kenshu <kenshu.app@gmail.com>
2021-11-18 15:11:57 +0900

    fix: 06 arrFunc

---
+++ 06/arrayFunc.php
@@ -0,0 +1,22 @@
+<?php
+$text = 'PHP　MySQL　Laravel';
+
+/* http://example.com?0=PHP&1=MySQL&2=Laravel */
+
+$alph = 'A-B-C';
+
+/* A | B | C | D | 4個 */
+
+$arr = explode('-', $alph);
+$num = array_push($arr, 'D');
+array_push($arr, $num . '個');
+$result = implode(' | ', $arr);
+// echo '<pre>';
+// print_r($arr);
+// echo '</pre>';
+
+// echo $num;
+?>
+
+<?= 'http://example.com?' . http_build_query(explode('　', $text))?><br>
+<?=$result?>
