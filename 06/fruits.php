fce686f4bf740b7590a4439a473b39790585f7a1
kenshu <kenshu.app@gmail.com>
2021-11-18 11:33:45 +0900

    fix: 06 fruits

---
+++ 06/fruits.php
@@ -0,0 +1,18 @@
+<?php
+$fruits = ['リンゴ', 'バナナ', 'ぶどう'];
+
+// echo $fruits[0] . '<br>';
+// echo $fruits[1] . '<br>';
+// echo $fruits[2];
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
+echo '<pre>';
+print_r($fruits);
+echo '</pre>';
+?>
+
+<?=$fruits[0]?><br>
+<?=$fruits[1]?><br>
+<?=$fruits[2]?>
