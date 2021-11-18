246b9fbc1a96af808c8df7bc138b58c4c3906caf
kenshu <kenshu.app@gmail.com>
2021-11-18 15:19:59 +0900

    update: 06 arrFunc2

--- 06/arrayFunc.php
+++ 06/arrayFunc.php
@@ -8,7 +8,9 @@ $alph = 'A-B-C';
 /* A | B | C | D | 4個 */
 
 $arr = explode('-', $alph);
-$num = array_push($arr, 'D');
+// $num = array_push($arr, 'D');
+array_push($arr, 'D');
+$num = count($arr);
 array_push($arr, $num . '個');
 $result = implode(' | ', $arr);
 // echo '<pre>';
