cba6185f78d9ae5fe9e68c956c8a4313a4513750
kenshu <kenshu.app@gmail.com>
2021-11-19 18:53:31 +0900

    fix: 09 select3

---
+++ 09/seletct2.php
@@ -0,0 +1,38 @@
+<?php
+$item = 'バナナ';
+if (!empty($_POST)) {
+    $item = $_POST['item'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php if ($_SERVER["REQUEST_METHOD"] === "POST") : ?>
+        <p><?=htmlspecialchars($item, ENT_QUOTES, 'UTF-8');?>が選択されました</p>
+    <?php endif; ?>
+    <form action="" method="post" novalidate>
+        <p>
+            <select name="item">
+                <option <?php if ($item === 'リンゴ') echo 'selected';?>>リンゴ</option>
+                <option <?php if ($item === 'バナナ') echo 'selected';?>>バナナ</option>
+                <option <?php if ($item === 'ブドウ') echo 'selected';?>>ブドウ</option>
+                <option <?= $item === 'メロン' ? 'selected' : '';?>>メロン</option>
+                <option <?= $item === 'みかん' ? 'selected' : '';?>>みかん</option>
+                <option <?= $item ?: 'selected'?>>さくらんぼ</option>
+                <option <?= $item ?: 'selected'?>>マンゴー</option>
+            </select>
+            <input type="submit" value="送信">
+        </p>
+    </form>
+</body>
+
+</html>
