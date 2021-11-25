45e9073c9d65b97087a54e8510398f43b8ff1124
kenshu <kenshu.app@gmail.com>
2021-11-25 11:18:44 +0900

    fix: 13 modify add getToday function

--- 13/modify.php
+++ 13/modify.php
@@ -1,5 +1,4 @@
 <?php
-$w = ['日', '月', '火', '水', '木', '金', '土'];
 $d1 = new DateTime();
 $d2 = new DateTime();
 $d1->modify('last day of February 2025');
@@ -9,10 +8,23 @@ $interval = $d1->diff($d2);
 $invert = $interval->invert;
 $days     = $interval->days;
 
+/**
+ * DateTimeのインスタンスを受けて
+ * 日付の日本語フォーマットを返す関数
+ *
+ * @param object $dayObj
+ * @return string
+ */
+function getToday(object $dayObj): string
+{
+    $w = ['日', '月', '火', '水', '木', '金', '土'];
+    return $dayObj->format('Y年m月d日') . '(' . $w[$dayObj->format('w')] . ')';
+}
+
 if ($days === 0) {
     echo '日付が同じです';
 } elseif ($invert === 1) {
-    echo $d1->format('Y年m月d日') . '(' . $w[$d1->format('w')] . ')の方が「' . $days . '日分」' . $d2->format('Y年m月d日') . '(' . $w[$d2->format('w')] . ')より新しいです';
+    echo getToday($d1) . 'の方が「' . $days . '日分」' . getToday($d2) . 'より新しいです';
 } else {
-    echo $d2->format('Y年m月d日') . '(' . $w[$d2->format('w')] . ')の方が「' . $days . '日分」' . $d1->format('Y年m月d日') . '(' . $w[$d1->format('w')] . ')より新しいです';
+    echo getToday($d2) . 'の方が「' . $days . '日分」' . getToday($d1) . 'より新しいです';
 }
